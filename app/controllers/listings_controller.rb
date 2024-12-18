a232d7716f537f11a73d5c062cf357891f0a7bfc
ccrawford4 <calumcrawford9@gmail.com>
2024-12-18 13:31:48 -0800

    working video

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,6 @@
 class ListingsController < ApplicationController
   def new
+    @logo_url = "https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/airbnb-logo-black-white-background.jpg"
     @current_step = params[:step] || 1
     render "new"
   end
