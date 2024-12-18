5211f23af05b164dcbe4c140a2a334e40dc9d4be
ccrawford4 <calumcrawford9@gmail.com>
2024-12-18 15:06:31 -0800

    uploaded rental type images successfully

--- scripts/insert_rental_types.rb
+++ scripts/insert_rental_types.rb
@@ -1,13 +1,13 @@
-# { name: 'House', image_url: '' },
 rental_types = [
-  { name: 'Apartment', image_url: '' },
-  { name: 'Barn', image_url: '' },
-  { name: 'Bed & breakfast', image_url: '' },
-  { name: 'Boat', image_url: '' },
-  { name: 'Cabin', image_url: '' },
-  { name: 'Camper/RV', image_url: '' },
-  { name: 'Casa particular', image_url: '' },
-  { name: 'Castle', image_url: '' }
+  { name: 'House', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/house.svg' },
+  { name: 'Apartment', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/apartment.svg' },
+  { name: 'Barn', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/barn.svg' },
+  { name: 'Bed & breakfast', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/bed%26breakfast.svg' },
+  { name: 'Boat', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/boat.svg' },
+  { name: 'Cabin', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/cabin.svg' },
+  { name: 'Camper/RV', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/camper%3Arv.svg' },
+  { name: 'Casa particular', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/casa-particular.svg' },
+  { name: 'Castle', image_url: 'https://airbnb-clone1.s3.us-west-1.amazonaws.com/protected-assets/castle.svg' }
 ]
 
 rental_types.each do |rental_type|
