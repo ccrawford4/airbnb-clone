63a07c5dac35b6d673de41922aedf25cf75e889c
ccrawford4 <calumcrawford9@gmail.com>
2025-01-02 22:59:23 -0500

    working create rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -12,13 +12,46 @@ class RentalsController < ApplicationController
     @rental.rental_images.build
   end
 
+  def edit
+    @rental = Rental.find(params[:id])
+    @rental_image = RentalImage.find_by(rental_id: @rental.id)
+    if @rental_image.nil?
+      render "error"
+    end
+  end
+
   def create
-    @rental = Rental.new(rental_params)
+    @rental = Rental.create(rental_params)
     if @rental.save
       redirect_to root_path, notice: "Rental was successfully created."
     else
       render :new
     end
+  end
+
+  def update
+    @rental = Rental.find(params[:id])
+    @rental_image = RentalImage.find_by(rental_id: @rental.id)
+
+    ActiveRecord::Base.transaction do
+      # Extract nested attributes for the first rental image
+      image_attributes = rental_params[:rental_images_attributes]["0"]
+
+      if @rental_image.update(
+        is_cover: image_attributes[:is_cover],
+        description: image_attributes[:description]
+      )
+        if @rental.update(rental_params.except(:rental_images_attributes))
+          redirect_to show, notice: "Rental was successfully updated."
+        else
+          raise ActiveRecord::Rollback
+        end
+      else
+        raise ActiveRecord::Rollback
+      end
+    end
+  rescue ActiveRecord::Rollback
+    render :edit
   end
 
   def destroy
@@ -41,7 +74,7 @@ class RentalsController < ApplicationController
       :score,
       :price,
       category_ids: [],
-      rental_images_attributes: [ :image, :is_cover ]
+      rental_images_attributes: [ :image, :is_cover, :description ]
     )
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   config.cache_store = :memory_store
 
   # Use Amazon S3 for Active Storage in development
-  config.active_storage.service = :amazon
-  # config.active_storage.service = :local
+  # config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
